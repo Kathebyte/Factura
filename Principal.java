fdb11b8fdc709b27f5562ee8bcfb4399e2653df6
EdPeReg <dardo1818@gmail.com>
2022-12-17 21:45:43 -0600

    Little corrections made to improve code readability and options 1 and 3 fixed

--- Principal.java
+++ Principal.java
@@ -1,125 +1,110 @@
 import java.util.Scanner;
-import java.util.Arrays;
-
 
 public class Principal {
-
-    public static int indiceProductoBarato(Producto producto[]){
+    public static int indiceProductoBarato(Producto productos[]){
         float precio;
         int indice = 0;
 
-        precio = producto[0].getValor();
-        for(int i= 1; i < producto.length;i++){
-            if(producto[i].getValor()<precio){
-                precio = producto[i].getValor();
-                indice= i;
+        // Lets assume the first product is the cheapest
+        precio = productos[0].getPrecio();
 
+        // Iterate starting from the index 1, index 0 is already taken
+        for(int i = 1; i < productos.length; i++){
+            // Update our index
+            if(productos[i].getPrecio() < precio){
+                precio = productos[i].getPrecio();
+                indice = i;
             }
-            
         }
         return indice;
-
     }
-    
 
-    public static int indiceProductoCostoso(Producto producto[]){
+    public static int indiceProductoCostoso(Producto productos[]) {
         float precio;
         int indice = 0;
 
-        precio = producto[0].getValor();
-        for(int i= 1; i > producto.length;i++){
-            if(producto[i].getValor()<precio){
-                precio = producto[i].getValor();
-                indice= i;
+        // Lets assume the first product is the most expensive
+        precio = productos[0].getPrecio();
 
+        // Iterate starting from the index 1, index 0 is already taken
+        for(int i = 1; i < productos.length; i++) {
+            // Update our index
+            if(productos[i].getPrecio() > precio) {
+                precio = productos[i].getPrecio();
+                indice = i;
             }
-            
         }
         return indice;
     }
 
     public static void main(String args[]) {
-        
         String nombreProducto;
-        int valor,opc=0;
-        int indiceBarato,indiceCostoso;
+        float precio;
+        int indice, opc = 0;
 
         Scanner teclado = new Scanner(System.in);
         
         System.out.println("Cuentos productos deseas Facturar");
         int numProd = teclado.nextInt();
 
-        Producto producto[] = new Producto[numProd];
+        Producto productos[] = new Producto[numProd];
 
-        for(int i =0; i< numProd; i++){
+        for(int i = 0; i < numProd; i++) {
             System.out.println("Ingresa el nombre del producto "+(i+1));
+            // This will allows us to read the string and not skip it
             teclado.nextLine();
             nombreProducto = teclado.nextLine();
             System.out.println("Introduzca el precio");
-            valor=teclado.nextInt();
+            precio=teclado.nextFloat();
             
-            producto[i] = new Producto(nombreProducto, valor);
+            productos[i] = new Producto(nombreProducto, precio);
         }
 
-        int i=0;
-        do{ System.out.println("MENU:");
-            System.out.println("1.Desea ingresar produtos y su respectivio valor");
-            System.out.println("2.Visualizar producto mas Barato"); 
-            System.out.println("3.Visualizar producto mas costoso"); 
-            System.out.println(".Conocer el total a pagar"); 
+        do {
+            System.out.println("MENU:");
+            System.out.println("1.Desea ingresar produtos y su respectivio precio");
+            System.out.println("2.Visualizar producto mas Barato");
+            System.out.println("3.Visualizar producto mas costoso");
+            System.out.println(".Conocer el total a pagar");
             System.out.println(".Facturar");
             System.out.println("4.Salir");
             System.out.println(".Digite la opcion del menu");
 
-            opc=teclado.nextInt();
+            opc = teclado.nextInt();
 
-        
-            switch(opc){
+            switch (opc) {
 
-                
-                case 1: System.out.println("Estos son los datos ingresados: ");
-                        producto[i].mostrarDatos();
-                        break;
+                case 1:
+                    System.out.println("Estos son los datos ingresados: ");
+                    // Loop in each product and show it
+                    for (int i = 0; i < productos.length; ++i) {
+                        System.out.println(productos[i].mostrarDatos());
+                    }
+                    break;
 
-                case 2: System.out.println("El producto mas Barato es : ");
-                        indiceBarato = indiceProductoBarato(producto);
-                        System.out.println("\nEl producto mas barato es: ");
-                        System.out.println(producto[indiceBarato].mostrarDatos());
-                        break;
+                case 2:
+                    System.out.println("El producto mas Barato es : ");
+                    indice = indiceProductoBarato(productos);
+                    System.out.println("\nEl producto mas barato es: ");
+                    System.out.println(productos[indice].mostrarDatos());
+                    break;
 
-                case 3: System.out.println("El producto mas Costoso es : ");
-                        indiceBarato = indiceProductoBarato(producto);
-                        System.out.println("\nEl producto mas barato es: ");
-                        System.out.println(producto[indiceBarato].mostrarDatos());
-                        break;
-                case 4: break;
-                default:System.out.println("Error, Opcion incorrecata");break;   
-                
+                case 3:
+                    System.out.println("El producto mas Costoso es : ");
+                    indice = indiceProductoCostoso(productos);
+                    System.out.println("\nEl producto mas barato es: ");
+                    System.out.println(productos[indice].mostrarDatos());
+                    break;
+                case 4:
+                    break;
+                default:
+                    System.out.println("Error, Opcion incorrecata");
+                    break;
+
             }
             System.out.println("");
-            i++;    
         }while(opc!=4);
+
+        teclado.close();
     }   
 }
-
-
-
-
-    
-    
-
-       
-            
-         // int suma= Arrays.stream(producto(valor)).sum();
-        // System.out.println(suma);
-            
-            
-        
-
-        
-
-        
-
-    
-    
-
